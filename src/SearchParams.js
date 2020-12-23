1af93895fe57fc8b8d3f37e389c89537e88582bc
frostz <aidenfrostbite@gmail.com>
2020-12-23 11:02:33 +0530

    Initial commit

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
 import Results from "./Results";
 import useDropdown from "./useDropdown";
+import ThemeContext from "./ThemeContext";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
